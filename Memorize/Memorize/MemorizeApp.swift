5b8a13968617b208bbd254b8e7134e8a32ab3b09
joaobatista99 <37956952+joaobatista99@users.noreply.github.com>
2023-03-05 11:10:37 -0300

    projeto criado

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by João Victor Batista on 05/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
